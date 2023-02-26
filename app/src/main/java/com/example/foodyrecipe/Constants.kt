53023ed91d40db05c942ffbb7173aaf1d9b84df2
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-02-26 12:50:43 +0300

    created constants folder and added base url,api key

---
+++ app/src/main/java/com/example/foodyrecipe/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foodyrecipe
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "8ba1b00a5d444985a31d922185edc78b"
+    }
+}
