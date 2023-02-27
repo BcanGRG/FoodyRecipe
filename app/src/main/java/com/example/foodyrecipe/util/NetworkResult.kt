757fdfcb2a629e3d9b43a7cc34ecaa3d2f5147fd
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-02-27 15:43:58 +0300

    constants was moved to util package and created NetworkResult sealed class

---
+++ app/src/main/java/com/example/foodyrecipe/util/NetworkResult.kt
@@ -0,0 +1,12 @@
+package com.example.foodyrecipe.util
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    class Success<T>(data: T) : NetworkResult<T>(data)
+    class Error<T>(data: T? = null, message: String?) : NetworkResult<T>(data, message)
+    class Loading<T>() : NetworkResult<T>()
+
+}
