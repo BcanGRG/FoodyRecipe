d9c38ec6f06db42668a56be382d6d01d8b9f76b8
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-02-27 15:44:30 +0300

    inject parameter converted to public

---
+++ app/src/main/java/com/example/foodyrecipe/data/Repository.kt
@@ -0,0 +1,11 @@
+package com.example.foodyrecipe.data
+
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+class Repository @Inject constructor(
+    val remoteDataSource: RemoteDataSource
+) {
+
+}
