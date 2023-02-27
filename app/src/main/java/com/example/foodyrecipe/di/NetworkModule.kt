10b3f0ebcb0bac512e5c66b8d20cca3d4fb2a086
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-02-27 15:46:54 +0300

    created di package and networkmodule dependencies

---
+++ app/src/main/java/com/example/foodyrecipe/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.example.foodyrecipe.di
+
+import com.example.foodyrecipe.util.Constants.Companion.BASE_URL
+import com.example.foodyrecipe.data.network.FoodRecipesApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(15, TimeUnit.SECONDS)
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi =
+        retrofit.create(FoodRecipesApi::class.java)
+
+}
