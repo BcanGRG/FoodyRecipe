f9d80ece24682721c9433f4144c708e629166679
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-01 03:20:40 +0300

    added api query keys constant values

--- app/src/main/java/com/example/foodyrecipe/util/Constants.kt
+++ app/src/main/java/com/example/foodyrecipe/util/Constants.kt
@@ -4,5 +4,13 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "8ba1b00a5d444985a31d922185edc78b"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
     }
 }
