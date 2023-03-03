0a0b5ba659595a7a1db2b2d7ca75ad8ea890f55f
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-03 03:06:32 +0300

    created RecipesTypeConverter.kt and added toJson-fromJson functions

---
+++ app/src/main/java/com/example/foodyrecipe/data/database/RecipesTypeConverter.kt
@@ -0,0 +1,21 @@
+package com.example.foodyrecipe.data.database
+
+import androidx.room.TypeConverter
+import com.example.foodyrecipe.models.FoodRecipe
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import javax.inject.Inject
+
+class RecipesTypeConverter @Inject constructor(private val gson: Gson) {
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+}
