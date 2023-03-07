a6f05b1cb9ef193bb9c1b951240bd9b2aaf0dd18
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-07 15:07:21 +0300

    created recipes binding and two binding adapter functions for image and text views

---
+++ app/src/main/java/com/example/foodyrecipe/bindingadapters/RecipesBinding.kt
@@ -0,0 +1,49 @@
+package com.example.foodyrecipe.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.foodyrecipe.data.database.RecipesEntity
+import com.example.foodyrecipe.models.FoodRecipe
+import com.example.foodyrecipe.util.NetworkResult
+
+class RecipesBinding {
+
+    companion object {
+
+        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+        @JvmStatic
+        fun errorImageViewVisibility(
+            imageView: ImageView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                imageView.visibility = View.VISIBLE
+            } else if (apiResponse is NetworkResult.Loading) {
+                imageView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+
+
+        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
+        @JvmStatic
+        fun errorTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString()
+            } else if (apiResponse is NetworkResult.Loading) {
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
