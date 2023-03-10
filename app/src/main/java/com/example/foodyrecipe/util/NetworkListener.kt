6d1a1f330101e92f61d4ac8493e2c8b787f0f8e6
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-10 17:36:10 +0300

    created NetworkListener class

---
+++ app/src/main/java/com/example/foodyrecipe/util/NetworkListener.kt
@@ -0,0 +1,36 @@
+package com.example.foodyrecipe.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class NetworkListener : ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable = MutableStateFlow(false)
+
+    fun checkNetworkAvailability(context: Context): MutableStateFlow<Boolean> {
+        var isConnected = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        networkCapabilities.let {
+            if (it!!.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+                isConnected = true
+            }
+        }
+
+        isNetworkAvailable.value = isConnected
+        return isNetworkAvailable
+    }
+
+    override fun onAvailable(network: Network) {
+        isNetworkAvailable.value = true
+    }
+
+    override fun onLost(network: Network) {
+        isNetworkAvailable.value = false
+    }
+}
